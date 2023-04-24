4b5cae2b4db3dfe52054f68e586135ada94fc921
athenacats <124017569+athenacats@users.noreply.github.com>
2023-04-24 09:03:47 +0300

    Add initial code to add title to header

---
+++ src/modules/homepage.js
@@ -0,0 +1,10 @@
+const header = document.createElement("div");
+const title = document.createElement("div");
+title.textContent = "Chachu Fermented Foods";
+title.classList.add("title");
+header.classList.add("header");
+header.appendChild(title);
+
+// const navigation = document.createElement("navigation");
+
+export default header;
