a4c167ae72dcb61857bad1d828c9470f8c8eb2f3
athenacats <124017569+athenacats@users.noreply.github.com>
2023-04-24 11:15:45 +0300

    Add navigation div

--- src/modules/homepage.js
+++ src/modules/homepage.js
@@ -5,6 +5,23 @@ title.classList.add("title");
 header.classList.add("header");
 header.appendChild(title);
 
-// const navigation = document.createElement("navigation");
+const navigation = document.createElement("div");
+navigation.classList.add("navigation");
+const ul = document.createElement("ul");
+navigation.appendChild(ul);
+const li1 = document.createElement("li");
+const li2 = document.createElement("li");
+const li3 = document.createElement("li");
+const text1 = document.createTextNode("Home");
+const text2 = document.createTextNode("Menu");
+const text3 = document.createTextNode("Contact");
+li1.appendChild(text1);
+li2.appendChild(text2);
+li3.appendChild(text3);
+ul.appendChild(li1);
+ul.appendChild(li2);
+ul.appendChild(li3);
+
+header.appendChild(navigation);
 
 export default header;
