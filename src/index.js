23fbd64d633f1a3a7cd97d120895414e54389bd7
athenacats <124017569+athenacats@users.noreply.github.com>
2023-04-25 17:50:43 +0300

    Change the functions in homepage to under umbrella

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import { header, container } from "./modules/homepage";
+// import defaultPage from "./loadpage";
+// import home from "./home";
+// import menu from "./menu";
+// import contact from "./contact";
+
+import homepage from "./modules/homepage";
 
 import "./style.scss";
 /* function component() {
   const content = document.getElementById("content");
   content.appendChild(header);
 } */
-
-const content = document.getElementById("content");
-content.appendChild(header);
-content.appendChild(container);
+homepage();
--- src/modules/homepage.js
+++ src/modules/homepage.js
@@ -1,79 +1,81 @@
-import menu from "./menu";
+function homepage() {
+  const header = document.createElement("div");
+  const title = document.createElement("h1");
+  title.textContent = "Chachu Fermented Foods";
+  title.classList.add("title");
+  header.classList.add("header");
+  header.appendChild(title);
 
-const header = document.createElement("div");
-const title = document.createElement("h1");
-title.textContent = "Chachu Fermented Foods";
-title.classList.add("title");
-header.classList.add("header");
-header.appendChild(title);
+  const navigation = document.createElement("div");
+  navigation.classList.add("navigation");
+  const ul = document.createElement("ul");
+  navigation.appendChild(ul);
+  const li1 = document.createElement("li");
+  li1.classList.add("menuItem");
+  const li2 = document.createElement("li");
+  li2.classList.add("menuItem");
+  const li3 = document.createElement("li");
+  li3.classList.add("menuItem");
+  const text1 = document.createTextNode("Home");
+  const text2 = document.createTextNode("Menu");
 
-const navigation = document.createElement("div");
-navigation.classList.add("navigation");
-const ul = document.createElement("ul");
-navigation.appendChild(ul);
-const li1 = document.createElement("li");
-li1.classList.add("menuItem");
-const li2 = document.createElement("li");
-li2.classList.add("menuItem");
-const li3 = document.createElement("li");
-li3.classList.add("menuItem");
-const text1 = document.createTextNode("Home");
-const text2 = document.createTextNode("Menu");
+  const text3 = document.createTextNode("Contact");
+  li1.appendChild(text1);
+  li2.appendChild(text2);
+  li3.appendChild(text3);
+  ul.appendChild(li1);
+  ul.appendChild(li2);
+  ul.appendChild(li3);
+  header.appendChild(navigation);
 
-text2.addEventListener("click", menu);
+  const container = document.createElement("div");
+  container.classList.add("container");
 
-const text3 = document.createTextNode("Contact");
-li1.appendChild(text1);
-li2.appendChild(text2);
-li3.appendChild(text3);
-ul.appendChild(li1);
-ul.appendChild(li2);
-ul.appendChild(li3);
-header.appendChild(navigation);
+  const about = document.createElement("div");
+  about.classList.add("about");
 
-const container = document.createElement("div");
-container.classList.add("container");
+  const aboutTitle = document.createElement("h3");
+  aboutTitle.classList.add("aboutTitle");
+  aboutTitle.textContent = "Why Eat Fermented Foods?";
+  about.appendChild(aboutTitle);
 
-const about = document.createElement("div");
-about.classList.add("about");
+  const aboutText1 = document.createElement("p");
+  aboutText1.classList.add("aboutText1");
+  aboutText1.textContent =
+    "Fermented foods are packed with probiotics—the good bacteria grow during the fermentation process—and eating them is one way to boost your gut health (eating more foods that are high in fiber, particularly prebiotic-rich foods, is important for gut health, too).";
+  about.appendChild(aboutText1);
 
-const aboutTitle = document.createElement("h3");
-aboutTitle.classList.add("aboutTitle");
-aboutTitle.textContent = "Why Eat Fermented Foods?";
-about.appendChild(aboutTitle);
+  const aboutText2 = document.createElement("p");
+  aboutText2.classList.add("aboutText2");
+  aboutText2.textContent =
+    "Check out the different gut friendly foods we have below!";
+  about.appendChild(aboutText2);
 
-const aboutText1 = document.createElement("p");
-aboutText1.classList.add("aboutText1");
-aboutText1.textContent =
-  "Fermented foods are packed with probiotics—the good bacteria grow during the fermentation process—and eating them is one way to boost your gut health (eating more foods that are high in fiber, particularly prebiotic-rich foods, is important for gut health, too).";
-about.appendChild(aboutText1);
+  const buttonBar = document.createElement("div");
+  buttonBar.classList.add("buttonBar");
 
-const aboutText2 = document.createElement("p");
-aboutText2.classList.add("aboutText2");
-aboutText2.textContent =
-  "Check out the different gut friendly foods we have below!";
-about.appendChild(aboutText2);
+  const button1 = document.createElement("button");
+  button1.classList.add("button");
+  button1.textContent = "Pastries";
+  // button1.addEventListener("click", () => {
+  buttonBar.appendChild(button1);
 
-const buttonBar = document.createElement("div");
-buttonBar.classList.add("buttonBar");
+  const button2 = document.createElement("button");
+  button2.classList.add("button");
+  button2.textContent = "Sides";
+  buttonBar.appendChild(button2);
 
-const button1 = document.createElement("button");
-button1.classList.add("button");
-button1.textContent = "Pastries";
-// button1.addEventListener("click", () => {
-buttonBar.appendChild(button1);
+  const button3 = document.createElement("button");
+  button3.classList.add("button");
+  button3.textContent = "Drinks";
+  buttonBar.appendChild(button3);
 
-const button2 = document.createElement("button");
-button2.classList.add("button");
-button2.textContent = "Sides";
-buttonBar.appendChild(button2);
+  about.appendChild(buttonBar);
+  container.appendChild(about);
 
-const button3 = document.createElement("button");
-button3.classList.add("button");
-button3.textContent = "Drinks";
-buttonBar.appendChild(button3);
+  const content = document.getElementById("content");
+  content.appendChild(header);
+  content.appendChild(container);
+}
 
-about.appendChild(buttonBar);
-container.appendChild(about);
-
-export { header, container };
+export default homepage;
