b989bebb967e81b1cab6c95ae52f67b46711dbdc
athenacats <124017569+athenacats@users.noreply.github.com>
2023-04-26 14:51:37 +0300

    Add dynamic date to footer

--- src/modules/footer.js
+++ src/modules/footer.js
@@ -1,8 +1,11 @@
 function footerPage() {
   const footer = document.createElement("div");
   footer.classList.add("footer");
-  const text4 = document.createTextNode("Copyright © 2017");
-  footer.appendChild(text4);
+  const date = new Date().getFullYear();
+  const dateString = date.toString();
+  const text1 = "Copyright © ".concat(dateString);
+  const text2 = document.createTextNode(text1);
+  footer.appendChild(text2);
 
   const content = document.getElementById("content");
 
