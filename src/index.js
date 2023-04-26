6c60481ad3fe1db75b606a76a80d8238589fbd8e
athenacats <124017569+athenacats@users.noreply.github.com>
2023-04-26 17:28:12 +0300

    Add function clicks to button area so buttons work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import defaultPage from "./modules/defaultPage";
+import clearPage from "./modules/clearfunction";
 import homepage from "./modules/homepage";
 import footerPage from "./modules/footer";
-import clearPage from "./modules/clearfunction";
 import menu from "./modules/menu";
 import contactPage from "./modules/contact";
 
@@ -14,26 +14,32 @@ defaultPage();
 homepage();
 footerPage();
 
-const homeLink = document.querySelector(".homeLink");
-homeLink.addEventListener("click", () => {
-  clearPage();
-  defaultPage();
-  homepage();
-  footerPage();
-});
+function clicks() {
+  const homeLink = document.querySelector(".homeLink");
+  homeLink.addEventListener("click", () => {
+    clearPage();
+    defaultPage();
+    homepage();
+    footerPage();
+    clicks();
+  });
 
-const menuLink = document.querySelector(".menuLink");
-menuLink.addEventListener("click", () => {
-  clearPage();
-  defaultPage();
-  menu();
-  footerPage();
-});
+  const menuLink = document.querySelector(".menuLink");
+  menuLink.addEventListener("click", () => {
+    clearPage();
+    defaultPage();
+    menu();
+    footerPage();
+    clicks();
+  });
 
-const contactLink = document.querySelector(".contactLink");
-contactLink.addEventListener("click", () => {
-  clearPage();
-  defaultPage();
-  contactPage();
-  footerPage();
-});
+  const contactLink = document.querySelector(".contactLink");
+  contactLink.addEventListener("click", () => {
+    clearPage();
+    defaultPage();
+    contactPage();
+    footerPage();
+    clicks();
+  });
+}
+clicks();
